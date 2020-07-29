102f8700afd011afe4cb2dc3ca8e702b88fe4585
Dipesh <dipesh.sakharkar@otiselevator.com>
2020-07-29 22:15:47 +0530

    Redux setup action reducer & flatlist to print list

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import {View,Text,ScrollView,TextInput,Button,StyleSheet} from 'react-native'
+import {useDispatch} from 'react-redux'
+
+
+import Colors from '../constants/Colors';
+import * as placesActions from '../store/places-actions'
+
+ const NewPlaceScreen=props=> {
+    const [titleValue,setTitleValue] = useState('');
+    
+    const dispatch =useDispatch()
+    const titleChangeHandler=text=>{
+        //add restrictions
+        setTitleValue(text)
+    }
+    
+    const savePlaceHandler =()=>{
+          dispatch(placesActions.addPlace(titleValue))
+          props.navigation.goBack();
+    }
+
+    return (
+    <ScrollView>
+        <View style={styles.form} >
+            <Text style={styles.label} >Title</Text>
+            <TextInput style={styles.textInput}  onChangeText={titleChangeHandler} value={titleValue}/>
+            <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler}/>
+        </View>
+    </ScrollView>
+    )
+}
+
+NewPlaceScreen.navigationOptions = {
+    headerTitle:'Add Place'
+}
+const styles = StyleSheet.create({
+    form:{
+        margin:30
+    },
+    label:{
+       fontSize:18,
+       marginBottom:15
+    },
+    textInput:{
+       borderBottomColor:"#ccc",
+       borderBottomWidth:1,
+       marginBottom:15,
+       paddingVertical:4,
+       paddingHorizontal:2
+    }
+})
+
+export default NewPlaceScreen;
